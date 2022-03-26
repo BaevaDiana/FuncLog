aa061bb7217b9fbe87f1ae4b8e4699d9fd5d6f47
BaevaDiana <thedianab15@gmail.com>
2022-03-26 15:34:34 +0300

    5.12

---
+++ 5.12/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let Otvet (k:string) =    
+   match k with 
+   |"F#"|"Prolog" -> "Поздравляю, Вы - подлиза!"
+   |"С++" -> "Ты молодец!"
+   |"Pascal" -> "Изучай новое!"
+   |"C#" ->"Серьезные вещи!"
+   |"Python" ->"Мощно!"
+   | _ -> "Интересный выбор..."
+
+[<EntryPoint>]
+let main argv =
+   System.Console.WriteLine("Какой ваш любимый язык программирования?")
+   //Случай суперпозиции
+   (Console.ReadLine>>Otvet>>Console.WriteLine)()
+   System.Console.WriteLine("Какой ваш любимый язык программирования?")
+  // Случай каррирования
+   let F y x z =z(x(y()))
+   F Console.ReadLine Otvet Console.WriteLine
+   0 
