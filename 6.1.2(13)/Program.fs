bbd1b728d5b98256c19b53200316a8413900738e
BaevaDiana <thedianab15@gmail.com>
2022-03-31 22:28:16 +0300

    6.1.2(13)

---
+++ 6.1.2(13)/Program.fs
@@ -0,0 +1,33 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+//Найти индекс минимального элемента
+
+let MinEl list = 
+    let rec minel list min indM indEL =
+        match list with
+        |[]->indM
+        |h::tail -> 
+            let newMin =if h<=min then h else min
+            let newInd = if h<=min then indEL else indM
+            minel tail newMin newInd (indEL+1)
+    minel list list.Head 0 0
+
+[<EntryPoint>]
+let main argv =
+    let l = readData
+    Console.WriteLine("Индекс минимального элемента списка: ")
+    Console.WriteLine(List.findIndex (fun x -> x=MinEl l) l)
+    0
