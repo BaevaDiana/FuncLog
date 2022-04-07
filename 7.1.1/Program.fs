d842498f33a6c20f7c401ee8eed517776a2016d6
BaevaDiana <thedianab15@gmail.com>
2022-04-07 21:14:06 +0300

    7.1.1(1)

---
+++ 7.1.1/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+
+let rec readList n = 
+    if n=0 then []
+    else
+    let Head = System.Convert.ToInt32(System.Console.ReadLine())
+    let Tail = readList (n-1)
+    Head::Tail
+
+let readData = 
+    Console.WriteLine("Введите размерность списка:  ")
+    let n=System.Convert.ToInt32(System.Console.ReadLine())
+    Console.WriteLine("Введите список: ")
+    readList n
+
+let rec writeList = function
+    [] ->   let z = System.Console.ReadKey()
+            0
+    | (head : int)::tail -> 
+                       System.Console.WriteLine(head)
+                       writeList tail
+
+//Дан целочисленный массив. Необходимо найти количество элементов, расположенных после последнего максимального.
+
+let CountAfterMax list = List.length list - (List.findIndexBack (fun x-> x=(List.max list) ) list)-1 
+
+[<EntryPoint>]
+let main argv =
+   Console.WriteLine("Количество элементов после последнего максимального:")
+   readData|> CountAfterMax  |>Console.WriteLine;
+    0 
